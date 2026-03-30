d4419d987fdf51791ee6c661bd0be6a327ef61dc
Jaehyun So <socowjh@gmail.com>
2026-03-30 13:58:16 +0900

    Time: 5 ms (34.95%), Space: 55.8 MB (52.28%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,33 @@
+function isValid(s) {
+  /**
+   * s = "([])"
+   *
+   * char='(': 여는 괄호 → stack.push('(') → stack=['(']
+   * char='[': 여는 괄호 → stack.push('[') → stack=['(', '[']
+   * char=']': 닫는 괄호 → stack.pop()='[', 짝 맞음 ✅ → stack=['(']
+   * char=')': 닫는 괄호 → stack.pop()='(', 짝 맞음 ✅ → stack=[]
+   *
+   * stack 비었음 → return true
+   */
+  const stack = [];
+  const mapping = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  for (const char of s) {
+    if (char in mapping) {
+      // 닫는 괄호
+      if (stack.length === 0 || stack[stack.length - 1] !== mapping[char]) {
+        return false;
+      }
+      stack.pop();
+    } else {
+      // 여는 괄호
+      stack.push(char);
+    }
+  }
+
+  return stack.length == 0;
+}
