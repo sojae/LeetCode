dd40d938dd65c4f9cf1c8a39f3c7bcc8ed0a7395
Jaehyun So <socowjh@gmail.com>
2026-03-31 11:23:35 +0900

    Time: 59 ms (5.43%), Space: 62.2 MB (5.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -18,13 +18,17 @@ function isValid(s) {
 
   for(const char of s){
     if(char in mapping){
+        //  mapping 안에 char 가 키값으로 있는지
+        console.log(mapping[char])
         if(stack.length == 0 || stack[stack.length -1] !== mapping[char]) return false
+        // 만약 있는데 스택이 비어있거나 스택 맨위에거랑 mapping밸류값이랑 다르면 false 
         stack.pop()
+        // 맨위꺼 꺼내기
     }else{
         stack.push(char)
+        // mapping안에 char라는 키값이 없으면 스택에 푸시
     }
   }
   return stack.length === 0
-
-
+//   스택이 비어있으면 true 짝이 다맞는거니까
 }
