7558ebbb56d5535cbf06b1d0bae6fd952e6b10c1
Jaehyun So <socowjh@gmail.com>
2026-04-01 16:25:17 +0900

    Time: 0 ms (100%), Space: 58.6 MB (15.23%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,9 +4,25 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i]===target){
-            return i
+    // for(let i = 0; i < nums.length; i++){
+    //     if(nums[i]===target){
+    //         return i
+    //     }
+    // }
+    // return -1;
+    let left = 0;
+    let right = nums.length -1;
+
+    while(left <= right){
+        const mid = left + Math.floor((right - left)/2)
+
+        if(nums[mid]===target){
+            return mid;
+        }
+        if(nums[mid] < target){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
         }
     }
     return -1;
