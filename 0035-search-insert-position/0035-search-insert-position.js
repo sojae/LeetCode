db4d76b1b65eb6b9c43fd21e2d18f70ed0b80642
Jaehyun So <socowjh@gmail.com>
2026-04-02 10:38:02 +0900

    Time: 0 ms (100%), Space: 51.8 MB (99.69%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -5,19 +5,24 @@
  */
 var searchInsert = function(nums, target) {
     let left = 0;
-    let right = nums.length -1
+    // 맨처음
+    let right = nums.length -1;
+    // 맨 마지막
 
     while(left <= right){
-        const mid = left + Math.floor((right - left)/2)
+        // 왼쪽보다 크거나 같아질때까지
+        const mid = left + Math.floor((right-left)/2);
 
         if(nums[mid] === target){
             return mid;
         }
 
         if(nums[mid] < target){
+            // 타겟 보다 작을경우 맨처음에서 가운데까지 버림
             left = mid + 1;
         }else{
-            right = mid -1;
+            // 타겟이 더 작을경우 맨마지막 부터 중간까지 버림
+            right = mid - 1;
         }
     }
     return left;
