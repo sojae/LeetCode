c7f245a682e67454d62c0b64970ba35bc7c24de3
Jaehyun So <socowjh@gmail.com>
2026-04-04 12:48:00 +0900

    Time: 1 ms (12.62%), Space: 53.6 MB (70.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,37 +4,24 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    // 시간복잡도 0(log n)
-    // 공간복잡도 0(1)
-    let left = 0; 
-    // 탐색 시작 위치 (맨 앞)
+    // 시간복잡도 O(log n)
+    // 공간복잡도 O(1)
 
-    let right = nums.length - 1; 
-    // 탐색 끝 위치 (맨 뒤)
+    let left = 0;
+    let right = nums.length -1;
 
-    while (left <= right) {
-        // 탐색 범위가 남아있는 동안 반복
-
+    while(left <= right){
         const mid = left + Math.floor((right - left) / 2);
-        // 현재 탐색 범위의 가운데 인덱스
 
-        if (nums[mid] === target) {
-            // target을 찾은 경우
+        if(nums[mid] === target){
             return mid;
         }
 
-        if (nums[mid] < target) {
-            // target이 더 크므로
-            // mid 포함 왼쪽 부분을 버림
-            left = mid + 1;
-        } else {
-            // target이 더 작으므로
-            // mid 포함 오른쪽 부분을 버림
-            right = mid - 1;
+        if(nums[mid] < target){
+            left += 1;
+        }else{
+            right -= 1;
         }
     }
-
-    // target이 없는 경우
-    // left가 들어갈 위치가 됨
     return left;
 };
