09451c8c47ce90a5c6c6717f5e8705ff519debf3
Jaehyun So <socowjh@gmail.com>
2026-04-04 12:45:32 +0900

    Time: 1 ms (10.11%), Space: 57.7 MB (79.67%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,27 +4,24 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    // for(let i = 0; i < nums.length; i++){
-    //     if(nums[i]===target){
-    //         return i
-    //     }
-    // }
-    // return -1;
-    // 시간복잡도 0(log n) 
-    // 공간복잡도 0(1)
+    // 시간복잡도 O(log n)
+    // 공간복잡도
+    // 풀이
+    // 맨 끝과 맨마지막을 구하고 중간값을 구해서 중간값이 타겟보다 작으면 왼쪽을 잘라내고 중간값+1 크면 오른쪽잘라내고 중간값 -1
     let left = 0;
     let right = nums.length -1;
 
     while(left <= right){
-        const mid = left + Math.floor((right - left)/2)
+        const mid = left + Math.floor((right - left) / 2);
 
-        if(nums[mid]===target){
+        if(nums[mid] === target){
             return mid;
         }
+
         if(nums[mid] < target){
-            left = mid + 1;
+            left += 1;
         }else{
-            right = mid - 1;
+            right -= 1;
         }
     }
     return -1;
