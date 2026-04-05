2c3c6c246e19c74dcf49ce6b4533621d38231561
Jaehyun So <socowjh@gmail.com>
2026-04-05 14:31:41 +0900

    Time: 0 ms (100%), Space: 54.6 MB (66.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,47 +4,47 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    const find = findFirst(nums,target)
+    const first = findFirst(nums,target)
     const last = findLast(nums,target)
 
-    return [find,last]
+    return [first, last]
 };
-function findFirst(nums, target){
-    left = 0;
-    right = nums.length -1;
-    answer = -1;
-    while(left <= right){
-        const mid = left + Math.floor((right - left) / 2)
-        
-        if(nums[mid] === target){
-            answer = mid;
-            right = mid -1;// 더 왼쪽 탐색 
-        }else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-        
-}
-return answer;}
 
-function findLast(nums, target){
-    left = 0;
-    right = nums.length -1;
-    answer = -1;
-    while(left <= right){
-        const mid = left + Math.floor((right - left) / 2)
-        
-        if(nums[mid] === target){
-            answer = mid;
-            left = mid +1;// 더 오른쪽 탐색 
-        }else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    
-}
-    return answer;}
-   
+function findFirst(nums,target){
+    let left = 0;
+    let right = nums.length -1;
+    let result = -1;
 
+    while(left <= right){
+        const mid = left + Math.floor((right - left) / 2);
+
+        if(nums[mid] === target){
+            result = mid;
+            right = mid -1;//“왼쪽에 더 같은 값 있나?”
+        }else if(nums[mid] < target){
+            left = mid +1;
+        }else{
+            right = mid -1;
+        }
+    }
+    return result;
+}
+function findLast(nums,target){
+    let left = 0;
+    let right = nums.length -1;
+    let result = -1;
+
+    while(left <= right){
+        const mid = left + Math.floor((right - left) / 2);
+
+        if(nums[mid] === target){
+            result = mid;
+             left = mid +1; //“오른쪽에 더 같은 값 있나?”
+        }else if(nums[mid] < target){
+            left = mid +1;
+        }else{
+            right = mid -1;
+        }
+    }
+    return result;
+}
