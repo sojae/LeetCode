174ce3274d317a152d8b1163752e14c00a2e81a5
Jaehyun So <socowjh@gmail.com>
2026-04-16 16:35:58 +0900

    Time: 50 ms (98.31%), Space: 65.2 MB (93.39%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -11,6 +11,18 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
+ /**
+ * 문제 내용
+ * 두 연결 리스트가 만나는 지점을 찾는 문제
+ *
+ * 문제 풀이 과정(아이디어)
+ * 1. pA는 listA, pB는 listB에서 시작
+ * 2. 끝에 도달하면 서로 반대 리스트로 이동
+ * 3. 길이가 맞춰지면서 결국 같은 지점에서 만남
+ *
+ * 시간복잡도: O(n + m)
+ * 공간복잡도: O(1)
+ */
 var getIntersectionNode = function(headA, headB) {
     let a = headA
     let b = headB
