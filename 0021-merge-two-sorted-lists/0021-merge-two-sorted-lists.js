833afc9a1f8854f76ccdd3b3835ef316829e0811
Jaehyun So <socowjh@gmail.com>
2026-04-16 15:57:13 +0900

    Time: 1 ms (33.87%), Space: 56.3 MB (87.69%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,15 +11,32 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if(list1 === null) return list2;
-    if(list2 === null) return list1;
+    // dfs
+    // if(list1 === null) return list2;
+    // if(list2 === null) return list1;
 
-    if(list1.val <= list2.val){
-        list1.next = mergeTwoLists(list1.next, list2)
-        return list1;
-    }else{
-        list2.next = mergeTwoLists(list1, list2.next)
-        return list2;
+    // if(list1.val <= list2.val){
+    //     list1.next = mergeTwoLists(list1.next, list2)
+    //     return list1;
+    // }else{
+    //     list2.next = mergeTwoLists(list1, list2.next)
+    //     return list2;
+    // }
+
+    // 포인터
+    const dummy = new ListNode()
+    let node = dummy
+
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            node.next = list1
+            list1 = list1.next
+        }else{
+            node.next = list2
+            list2 = list2.next
+        }
+        node = node.next
     }
-    
+    node.next = list1||list2
+    return dummy.next
 };
