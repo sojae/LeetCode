020f9ef258fe47f15234631b9cbd81669d6049e8
Jaehyun So <socowjh@gmail.com>
2026-06-01 18:32:21 +0900

    Time: 3 ms (56.06%), Space: 55.6 MB (27.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,31 +4,39 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // 브루트포스 (이중포문)
+    // 시간 복잡도 O(n2)
+    // 공간 복잡도 O(1)
     // for(let i = 0; i < nums.length; i++){
-    //     for(let j = i+1; j < nums.length; j++){
+    //     for(let j = i+1; j <nums.length; j++){
     //         if(nums[i]+nums[j]===target){
-    //             return [i,j]
+    //             return[i,j]
     //         }
     //     }
     // }
-    // return []
-    // const map ={}
+    
+    // 객체 사용법
+    // 시간 복잡도 O(n)
+    // 공간 복잡도 O(n)
+    const seen ={}
+    for(let i = 0; i < nums.length; i++){
+        const complement = target - nums[i]
+        if(complement in seen){
+            return [seen[complement],i]
+        }
+        seen[nums[i]]=i;
+    }
+    return[]
+    // 해시맵
+    // 시간 복잡도 O(n)
+    // 공간 복잡도 O(n)
+    // const seen = new Map()
     // for(let i = 0; i < nums.length; i++){
-    //     com = target - nums[i]
-
-    //     if(com in map){
-    //         return [map[com],i]
+    //     const complement = target - nums[i]
+    //     if(seen.has(complement)){
+    //         return[seen.get(complement),i]
     //     }
-    //     map[nums[i]]=i
+    //     seen.set(nums[i],i)
     // }
     // return []
-    const map = new Map()
-        for(let i = 0; i < nums.length; i++){
-        com = target - nums[i]
-        if(map.has(com)){
-            return [map.get(com),i]
-        }
-        map.set(nums[i],i)
-        }
-        return []
 };
