5210128b6b994a6ac6f8f7bbc00ea2fdf30b676d
Jaehyun So <socowjh@gmail.com>
2026-06-29 22:56:03 +0900

    Time: 0 ms (100%), Space: 57.9 MB (84.58%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    function dfs(node , min, max){
+        if(node === null)return true
+        if(node.val <= min)return false
+        if(node.val >= max)return false
+        return dfs(node.left, min, node.val) && dfs(node.right,node.val,max)
+    }
+    return dfs(root, -Infinity, Infinity)
+};
