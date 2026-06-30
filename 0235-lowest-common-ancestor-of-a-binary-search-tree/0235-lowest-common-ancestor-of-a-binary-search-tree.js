238949b22a5c9bb605cb21bd2ca0217867e78a6d
Jaehyun So <socowjh@gmail.com>
2026-06-30 14:05:38 +0900

    Time: 62 ms (59.5%), Space: 65.4 MB (33.85%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -16,11 +16,10 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     if(p.val < root.val && q.val < root.val){
-        return lowestCommonAncestor(root.left,p,q)
+        return lowestCommonAncestor(root.left , p ,q)
     }
     if(p.val > root.val && q.val > root.val){
-        return lowestCommonAncestor(root.right , p, q)
+        return lowestCommonAncestor(root.right, p,q)
     }
     return root
-    
 };
