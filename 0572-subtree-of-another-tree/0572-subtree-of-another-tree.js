e9da8d9d9bd3e3ede8cc2a796dce069d17559e27
Jaehyun So <socowjh@gmail.com>
2026-06-30 13:57:35 +0900

    Time: 4 ms (85.28%), Space: 61.4 MB (86.86%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -10,18 +10,20 @@
  * @param {TreeNode} root
  * @param {TreeNode} subRoot
  * @return {boolean}
+ 시간복잡도 O(n*m)
+ 공간복잡도 O(h)
  */
 var isSubtree = function(root, subRoot) {
-    if(root === null)return false
-    if(isSameTree(root,subRoot))return true
+    if(root === null)return false;
+    if(isSameTree(root,subRoot))return true;
+    
+    return isSubtree(root.left,subRoot) || isSubtree(root.right, subRoot)
 
-    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
-};
-
+}
 function isSameTree(p,q){
-    if(p === null && q === null)return true
-    if(p === null || q === null)return false
-    if(p.val !== q.val)return false
+    if(p === null && q === null)return true;
+    if(p === null || q === null)return false;
+    if(p.val !== q.val)return false;
 
-    return isSameTree(p.left,q.left) && isSameTree(p.right,q.right)
+    return isSameTree(p.left,q.left) && isSameTree(p.right, q.right)
 }
