47422c5bcf8266380fde7a5475d4c83fed65c4f8
Jaehyun So <socowjh@gmail.com>
2026-07-01 15:40:22 +0900

    Time: 38 ms (32.74%), Space: 70.4 MB (5.08%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -0,0 +1,81 @@
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+class MyCustomMinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    push(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+
+    pop() {
+        const top = this.heap[0];
+        const last = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this.bubbleDown();
+        }
+        return top;
+    }
+
+    bubbleUp() {
+        let i = this.heap.length - 1;
+        while (i > 0) {
+            const parent = Math.floor((i - 1) / 2);
+            if (this.heap[parent] <= this.heap[i]) break;
+            [this.heap[parent], this.heap[i]] = [this.heap[i], this.heap[parent]];
+            i = parent;
+        }
+    }
+
+    bubbleDown() {
+        let i = 0;
+        const n = this.heap.length;
+        while (true) {
+            let smallest = i;
+            const left = 2 * i + 1;
+            const right = 2 * i + 2;
+            if (left < n && this.heap[left] < this.heap[smallest]) smallest = left;
+            if (right < n && this.heap[right] < this.heap[smallest]) smallest = right;
+            if (smallest === i) break;
+            [this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]];
+            i = smallest;
+        }
+    }
+}
+var KthLargest = function(k, nums) {
+    this.k = k;
+    this.heap = new MyCustomMinHeap();
+    for (const num of nums) {
+        this.add(num);
+    }
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.heap.push(val);
+    if (this.heap.size() > this.k) {
+        this.heap.pop();
+    }
+    return this.heap.peek();
+};
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
