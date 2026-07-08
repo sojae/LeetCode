3e0286740f53fe0d39c6f71826f162654bdb4b84
Jaehyun So <socowjh@gmail.com>
2026-07-08 14:01:35 +0900

    Time: 299 ms (75.04%), Space: 54.1 MB (74.88%) - LeetHub

---
+++ 0079-word-search/0079-word-search.js
@@ -0,0 +1,36 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    const rows = board.length;
+    const cols = board[0].length;
+    
+    const dfs = (r,c,i) => {
+        if(i === word.length)return true
+
+        if(r < 0 || r >= rows)return false
+        if(c < 0 || c >= cols)return false
+        if(board[r][c] !== word[i])return false
+        if(board[r][c] === '#')return false
+
+        const temp = board[r][c]
+        board[r][c] = '#'
+
+        const found = dfs(r+1, c, i+1)||dfs(r-1, c, i+1)||dfs(r,c+1,i+1)||dfs(r,c-1,i+1)
+
+        board[r][c] = temp
+
+        return found
+
+    }
+
+    for(let r = 0; r < rows; r++ ){
+        for(let c = 0; c < cols; c++){
+            if(dfs(r,c,0))return true
+        }
+    }
+    return false
+
+};
