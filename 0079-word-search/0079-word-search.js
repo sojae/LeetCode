55a7da245fe7e305a5edea9933509458620d531b
Jaehyun So <socowjh@gmail.com>
2026-07-10 17:44:35 +0900

    Time: 305 ms (65.68%), Space: 55.4 MB (46.1%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -2,35 +2,63 @@
  * @param {character[][]} board
  * @param {string} word
  * @return {boolean}
- * 시간복잡도: O(m * n * 4^L) - L은 단어 길이
- * 공간복잡도: O(L) - 재귀 깊이
  */
 var exist = function(board, word) {
+    // 가로 세로열만들기
+    // 그래프 + 백트래킹 문제네
     const rows = board.length
     const cols = board[0].length
-    
+
     const dfs = (r,c,i) => {
         if(i === word.length)return true
-        if(r < 0 || r >= rows)return false
-        if(c < 0 || c >= cols)return false
+
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = 2 2 4 = 2 1 5 > i = 5 word.length 5 리턴 true
+        if(r < 0 || r >= rows||c < 0 || c >=cols)return false
         if(board[r][c] !== word[i])return false
+        
 
         const tmp = board[r][c]
+        // 0 0 0 tmp = a
+        // 0 0 0 = 0 1 1 tmp = b 
+        // 0 0 0 = 0 1 1  = 0 2 2  = tmp = c
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3 tmp = c
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = 2 2 4 = tmp = e
         board[r][c] = '#'
+        // 0 0 0 a = #
+        // 0 0 0 = 0 1 1 b = '#'
+        // 0 0 0 = 0 1 1  = 0 2 2  = c = '#'
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = c ='#'
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = 2 2 4 = e = '#'
+        const found = dfs(r+1,c,i+1)||
+        dfs(r-1,c,i+1)||
+        dfs(r,c+1,i+1)||
+        dfs(r,c-1,i+1)
+        // 0 0 0 = 1 0 1 리턴
+        // 0 0 0 = -1 0 1 리턴
+        // 0 0 0 = 0 1 1 b 
+        // 0 0 0 = 0 1 1 = 1 1 2 f c 틀리니까 리턴 
+        // 0 0 0 = 0 1 1 = -1 1 2 리턴
+        // 0 0 0 = 0 1 1  = 0 2 2 c
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3 c
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = 2 2 4 e
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = 2 2 4 = 325 리턴
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = 2 2 4 = 125 리턴
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = 2 2 4 = 235 리턴
+        // 0 0 0 = 0 1 1  = 0 2 2 c = 1 2 3  = 2 2 4 = 215 d
 
-        const found = dfs(r+1,c,i+1) || dfs(r-1,c,i+1)||dfs(r,c+1,i+1) || dfs(r,c-1,i+1)
+        // abcced
+        // '#'bce
+        // sfcs
+        // adee
+
 
         board[r][c] = tmp
         return found
-
     }
-
-    
     for(let r = 0; r < rows; r++){
         for(let c = 0; c < cols; c++){
             if(dfs(r,c,0))return true
         }
     }
     return false
-
 };
