f87b5090aa24cbc64c77501f447613efd51a1a5c
Jaehyun So <socowjh@gmail.com>
2026-07-20 21:28:51 +0900

    Time: 2 ms (68.41%), Space: 59.5 MB (18.74%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -1,12 +1,15 @@
 /**
  * @param {number[]} nums
  * @return {boolean}
+  시간복잡도 O(n)
+  공간복잡도 O(1)
  */
 var canJump = function(nums) {
     let maxNum = 0
-    for(let i = 0; i < nums.length;i++){
+
+    for(let i = 0; i < nums.length; i++){
         if(i > maxNum)return false
-        maxNum = Math.max(maxNum, i + nums[i]);
+        maxNum = Math.max(maxNum, i + nums[i])
     }
     return true
 };
