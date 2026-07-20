0cb7f92981e24237d54d1a7e5648585779787432
Jaehyun So <socowjh@gmail.com>
2026-07-20 19:01:25 +0900

    Time: 1 ms (87.49%), Space: 63.9 MB (48.87%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,18 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-//  새로시작 = 현재 숫자 = nums[i]
-// 이어가기 = 이전합 + 현재숫자 = currentNum + nums[i]
 
-// 이 둘을 비교해야 하니까
-// Math.max(nums[i], currentNum + nums[i])
 var maxSubArray = function(nums) {
-    let curSum = nums[0]
-    let maxSum = nums[0]
+    let curNum = nums[0]
+    let maxNum = nums[0]
+    // [5,4,-1,7,8]
     for(let i = 1; i < nums.length; i++){
-        curSum = Math.max(nums[i],curSum + nums[i])
-
-        maxSum = Math.max(maxSum,curSum)
+        curNum = Math.max(nums[i], curNum + nums[i])
+        // 
+        maxNum = Math.max(maxNum, curNum)
+       
     }
-    return maxSum
+    return maxNum
 };
