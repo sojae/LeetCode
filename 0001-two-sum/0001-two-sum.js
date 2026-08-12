72c96d3faf0fad062a4ecfff02a4acbdd4b3fb44
Jaehyun So <socowjh@gmail.com>
2026-08-12 10:52:24 +0500

    Time: 1 ms (88.78%), Space: 55.2 MB (41.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,39 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // 브루트포스 (이중포문)
-    // 시간 복잡도 O(n2)
-    // 공간 복잡도 O(1)
-    // for(let i = 0; i < nums.length; i++){
-    //     for(let j = i+1; j <nums.length; j++){
-    //         if(nums[i]+nums[j]===target){
-    //             return[i,j]
-    //         }
-    //     }
-    // }
-    
-    // 객체 사용법
     // 시간 복잡도 O(n)
-    // 공간 복잡도 O(n)
-    const seen ={}
+    // 공간 복잡도 O(n) 최악의 경우 배열 전체를 Map에 저장
+    const map = new Map() 
     for(let i = 0; i < nums.length; i++){
-        const complement = target - nums[i]
-        if(complement in seen){
-            return [seen[complement],i]
+        const com = target - nums[i]
+        if(map.has(com)){
+            return [map.get(com),i]
         }
-        seen[nums[i]]=i;
+        map.set(nums[i],i)
     }
     return[]
-    // 해시맵
-    // 시간 복잡도 O(n)
-    // 공간 복잡도 O(n)
-    // const seen = new Map()
-    // for(let i = 0; i < nums.length; i++){
-    //     const complement = target - nums[i]
-    //     if(seen.has(complement)){
-    //         return[seen.get(complement),i]
-    //     }
-    //     seen.set(nums[i],i)
-    // }
-    // return []
+    
 };
