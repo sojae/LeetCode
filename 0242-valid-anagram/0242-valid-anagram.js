f687a3fb3f1f9a21a208d81ffe45f9371f253d1c
Jaehyun So <socowjh@gmail.com>
2026-08-12 10:39:49 +0500

    Time: 9 ms (86.94%), Space: 59.5 MB (25.09%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    // 시간복잡도 O(n)
+    // 공간복잡도 O(1)
+    // count 객체에 문자 종류만큼 저장 알파벳은 최대 26개로 정해져 있음
+    if(s.length !== t.length)return false;
+
+    const count = {}
+    for(const char of s){
+        count[char] = (count[char]||0)+1
+    }
+
+    for(const char of t){
+        if(!count[char])return false
+        count[char]--
+    }
+    return true
+};
