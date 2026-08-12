87ede1fba84fbf854a64d027a21a8c3c4fb6a07e
Jaehyun So <socowjh@gmail.com>
2026-08-12 11:20:23 +0500

    Time: 29 ms (67.93%), Space: 68.9 MB (25.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,7 +4,33 @@
  */
 var groupAnagrams = function(strs) {
     // 시간 복잡도 O(n * k log k)
+    // 각 문자열마다 정렬 O(k log k)
+    // n개 문자열 순회
+    // → O(n × k log k)
     // 공간 복잡도 O(n * k)
+    // 모든 문자열을 map에 저장
+    // n개 문자열 × 평균 길이 k
+    // → O(n × k)
+
+    const map = new Map()
+
+    for(const str of strs){
+        const key = str.split('').sort().join('')
+
+        if(!map.has(key)){
+            map.set(key,[])
+        }
+        map.get(key).push(str)
+    }
+    return [...map.values()]
+
+
+
+
+
+
+
+
     // const map = {};
 
     // for(const str of strs){
@@ -18,17 +44,4 @@ var groupAnagrams = function(strs) {
     //     map[key].push(str)
     // }
     // return Object.values(map);
-
-
-    const map = new Map()
-
-    for(const str of strs){
-        const key = str.split('').sort().join('')
-
-        if(!map.has(key)){
-            map.set(key,[])
-        }
-        map.get(key).push(str)
-    }
-    return [...map.values()]
 };
